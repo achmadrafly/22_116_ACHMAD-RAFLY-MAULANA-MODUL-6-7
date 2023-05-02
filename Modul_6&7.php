8dcc98435e70edc6bf4de899a1ff40e2f50b84ce
achmadrafly <113223443+achmadrafly@users.noreply.github.com>
2023-05-02 19:23:27 +0700

    Add files via upload

---
+++ Modul_6&7.php
@@ -0,0 +1,52 @@
+<?php 
+	if(isset($_POST['hitung'])){
+		$bil1 = $_POST['bil1'];
+		$bil2 = $_POST['bil2'];
+		$operasi = $_POST['operasi'];
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $bil1+$bil2;
+			break;
+			case 'kurang':
+				$hasil = $bil1-$bil2;
+			break;
+			case 'kali':
+				$hasil = $bil1*$bil2;
+			break;
+			case 'bagi':
+				$hasil = $bil1/$bil2;
+			break;			
+		}
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kalkulator</title>
+	<link rel="stylesheet" type="text/css" href="style6&7.css">
+</head>
+<body>
+	<div class="kalkulator">
+		<h2 class="judul">KALKULATOR</h2>
+		<form method="post" action="Modul_6&7.php">			
+			<input type="text" name="bil1" class="bil">
+			<input type="text" name="bil2" class="bil">
+			<select class="opt" name="operasi">
+				<option value="tambah">+</option>
+				<option value="kurang">-</option>
+				<option value="kali">x</option>
+				<option value="bagi">/</option>
+			</select>
+			<input type="submit" name="hitung" value="Hitung" class="tombol">											
+		</form>
+		<?php if(isset($_POST['hitung'])){ ?>
+			<input type="text" value="<?php echo $hasil; ?>" class="bil">
+		<?php }else{ ?>
+			<input type="text" value="0" class="bil">
+		<?php } ?>			
+	</div>
+</body>
+</html>
+
+
+
